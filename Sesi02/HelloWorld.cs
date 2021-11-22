7471c884b74d4457190e73dfa0f2c08f0a67e8d2
ginwa123 <gilangtrisetya123@gmail.com>
2021-11-23 04:32:53 +0700

    add helloworld.cs in sesi02

---
+++ Sesi02/HelloWorld.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sesi02
+{
+    public class HelloWorld
+    {
+         public static void Main(String[] args)
+        {
+            string nama = "Gilang Tri";
+            Console.WriteLine("Hello World");
+            Console.WriteLine( nama);
+            Console.WriteLine("Selamat Datang di Program Bootcamp OCBC");
+            Console.WriteLine("Tahap 1 saya akan belajar C#");
+        }
+    }
+}
