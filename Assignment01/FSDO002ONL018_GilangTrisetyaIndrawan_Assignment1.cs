ad6cc06f9c63acda432dc78b9973a4bcc6a1ef04
ginwa123 <gilangtrisetya123@gmail.com>
2021-11-23 16:23:23 +0700

    update change to uppercase

--- Assignment01/FSDO002ONL018_GilangTrisetyaIndrawan_Assignment1.cs
+++ Assignment01/FSDO002ONL018_GilangTrisetyaIndrawan_Assignment1.cs
@@ -64,9 +64,9 @@ public class Assignment1
         bool isNotFinished = true;
         // array abjad
         char[] myLetter = {
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
             };
 
 
--- Assignment01/Soal1.cs
+++ Assignment01/Soal1.cs
@@ -8,10 +8,11 @@ public class Soal1
 
         // array abjad
         char[] myLetter = {
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
             };
+
 
         int inc = 0;
         Console.Write("masukkan tinggi max(26): ");
