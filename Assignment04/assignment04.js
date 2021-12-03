e1798f3848a24a281662adb4cac70a4dc85bf5b8
ginwa123 <gilangtrisetya123@gmail.com>
2021-12-03 13:49:24 +0700

    add assignment 04

---
+++ Assignment04/assignment04.js
@@ -0,0 +1,14 @@
+
+
+let number1 = 30
+let number2 = 20
+
+if (number1 !== undefined && number2 !== undefined) {
+    console.log(` Addition          :  ${number1} + ${number2} = ${number1+number2}`);
+    console.log(` Subtration        :  ${number1} - ${number2} = ${number1-number2}`);
+    console.log(` Multiplication    :  ${number1} * ${number2} = ${number1*number2}`);
+    console.log(` Division          :  ${number1} / ${number2} = ${number1/number2}`);
+    console.log(` Modulus           :  ${number1} % ${number2} = ${number1%number2}`);
+    console.log(` Increment         :  ${number1} dan ${number2} = ${++number1} ${++number2}`);
+    console.log(` Decrement         :  ${number1} dan ${number2} = ${--number1} ${--number2}`);
+}
